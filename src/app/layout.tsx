b1830dd3baf47990110e8c91a490c22e6ecf68c3
putintsev.sergii@gmail.com <sergii.putintsev@if.lv>
2025-05-31 00:43:07 +0300

    fixed root layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Knights of John Paul II',
+  description: 'Order of Knights of Saint John Paul II the Great',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+} 
